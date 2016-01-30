56c9b97fb297c61e61867e7e52c02b7e46ca9604
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-01-30 18:09:32 +0100

    set 1, ex 3

--- src/Set1.hs
+++ src/Set1.hs
@@ -33,3 +33,27 @@ randLetter seed = (toLetter $ fst r, snd r)
 
 randString3 :: String
 randString3 = map fst $ take 3 $ iterate (randLetter . snd) $ randLetter $ mkSeed 1
+
+-- More Generators
+
+-- WIP (not sure I captured the correct abstraction)
+
+type Gen a = Seed -> (a, Seed)
+myRand :: Gen Integer
+myRand = rand
+
+myRandLetter :: Gen Char
+myRandLetter = randLetter
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA f g seed = (f $ fst r, snd r)
+  where r = g seed
+
+randEven :: Gen Integer -- the output of rand * 2
+randEven = generalA (\x -> x * 2) myRand
+
+randOdd :: Gen Integer -- the output of rand * 2 + 1
+randOdd = generalA (\x -> x * 2 + 1) myRand
+
+randTen :: Gen Integer -- the output of rand * 10
+randTen = generalA (\x -> x * 10) myRand
