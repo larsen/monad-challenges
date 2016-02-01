5bf97d9bc9eab3677a3761f95bb18bd186b7c970
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-02-01 22:19:54 +0100

    missing exercise title

--- src/Set2.hs
+++ src/Set2.hs
@@ -5,8 +5,12 @@ module Set2 where
 
 import MCPrelude
 
+-- The Maybe Type
+
 data Maybe a = Just a | Nothing
 
 instance Show a => Show (Maybe a) where
   show (Just a) = "Just " ++ show a
   show Nothing  = "_"
+
+
