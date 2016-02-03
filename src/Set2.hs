3992404b212f1a3f96a68a224d82139c3f45bd7b
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-02-03 23:10:16 +0100

    set 2 ex 3, plus a bugfix in previous ex

--- src/Set2.hs
+++ src/Set2.hs
@@ -12,7 +12,7 @@ data Maybe a = Just a | Nothing
 
 instance Show a => Show (Maybe a) where
   show (Just a) = "Just " ++ show a
-  show Nothing  = "_"
+  show Nothing  = "Nothing"
 
 -- Build a library of things that fail
 
@@ -26,9 +26,9 @@ tailMay (_:t) = Just t
 
 lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
 lookupMay item [] = Nothing
-lookup item ((k,v):ks)
+lookupMay item ((k,v):ks)
   | item == k = Just v
-  | otherwise = lookup item ks
+  | otherwise = lookupMay item ks
 
 divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
 divMay _ 0 = Nothing
@@ -41,3 +41,25 @@ maximumMay l  = Just (maximum l)
 minimumMay :: Ord a => [a] -> Maybe a
 minimumMay [] = Nothing
 minimumMay l  = Just (minimum l)
+
+-- Chains of failing computations
+
+queryGreek :: GreekData -> String -> Maybe Double
+queryGreek greekData item =
+  case lookupMay item greekData of
+    Nothing -> Nothing
+    Just xs -> case tailMay xs of
+      Nothing -> Nothing
+      Just t -> case maximumMay t of
+        Nothing -> Nothing
+        Just m -> case headMay xs of
+          Nothing -> Nothing
+          Just h -> divMay (fromIntegral m) (fromIntegral h)
+
+-- check;
+-- queryGreek greekDataA "alpha" == Just 2.0
+-- queryGreek greekDataA "beta" == Nothing
+-- queryGreek greekDataA "gamma" == Just 3.3333333333333335
+-- queryGreek greekDataA "delta" == Nothing
+-- queryGreek greekDataA "zeta" == Nothing
+
