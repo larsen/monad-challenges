8f83c8f4d0d9a8ad402e54b54b7a798bae017cde
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-03-06 18:00:37 +0100

    set 3 ex 2

--- src/Set3.hs
+++ src/Set3.hs
@@ -12,3 +12,16 @@ allPairs (x:xs) ys = makePairs x ys ++ allPairs xs ys
     makePairs :: a -> [b] -> [(a, b)]
     makePairs _ [] = []
     makePairs x (y:ys) = (x,y) : makePairs x ys
+
+data Card = Card Int String
+
+instance Show Card where
+  show (Card rank suit) = show rank ++ suit
+
+allCards :: [Int] -> [String] -> [Card]
+allCards [] _ = []
+allCards (x:xs) ys = makeCards x ys ++ allCards xs ys
+  where
+    makeCards :: Int -> [String] -> [Card]
+    makeCards _ [] = []
+    makeCards x (y:ys) = (Card x y) : makeCards x ys
