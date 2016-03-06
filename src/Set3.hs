ca9355835053b8ca46127fe98a00aa1086b127e9
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-03-06 17:50:34 +0100

    another version of set 3 ex 1, this time more 'Monad Challenges "compliant"'

--- src/Set3.hs
+++ src/Set3.hs
@@ -1,9 +1,14 @@
--- {-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE MonadComprehensions #-}
 {-# LANGUAGE RebindableSyntax  #-}
 
 module Set3 where
 
--- import MCPrelude
+import MCPrelude
 
 allPairs :: [a] -> [b] -> [(a,b)]
-allPairs xs ys = [ (x,y) | x <- xs, y <- ys ]
+allPairs [] _ = []
+allPairs (x:xs) ys = makePairs x ys ++ allPairs xs ys
+  where
+    makePairs :: a -> [b] -> [(a, b)]
+    makePairs _ [] = []
+    makePairs x (y:ys) = (x,y) : makePairs x ys
